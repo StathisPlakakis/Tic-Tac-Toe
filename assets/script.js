054b691cba2a5800ff134b4d7a5cd95ddcb7d232
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-11 17:57:33 +0300

    Add player factory function and 2 player instances
    Every player has a mark and a method to maipulate the current board

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,23 @@ const board = (function () {
     }
 })();
 
+function player(mark) {
+    const markBoard = function (row, col) {
+        if (board.currentBoard[row][col] === null) {
+            board.currentBoard[row][col] = mark;
+            console.log(board.currentBoard);
+        }else {
+            console.log("It's full");
+            console.log(board.currentBoard);
+        }
+    }
 
+    return {
+        mark,
+        markBoard,
+    }
+}
 
 console.log(board.currentBoard);
+const player1 = player("X");
+const player2 = player("O");
