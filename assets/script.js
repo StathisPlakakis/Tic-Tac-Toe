58af9306c4ffc3d4b13344e9466d3eabd828a7ca
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-12 17:12:51 +0300

    Add a liitle logic to the game object
    Now playGame function let player and pc manipulate board's state

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ const board = (function () {
         [null, null, null],
         [null, null, null], 
 ];
+
+    
     return {
         currentBoard,
     }
@@ -13,6 +15,30 @@ const game = (function () {
     const playGame = function () {
         console.log("Let's Play!!")
         console.log(board.currentBoard);
+        let plays = human;
+        while (true) {
+            console.log(`${plays.name} plays`);
+            if (plays === human) {
+                let row = prompt("row: ");
+                let col = prompt("col: ");
+                while (board.currentBoard[row][col] !== null) {
+                    console.log("This cell is full pick another")
+                    row = prompt("row: "); 
+                    col = prompt("col: ");
+               }
+                plays.markBoard(row, col);
+                plays = computer;
+            }else {
+                let row = Math.floor(Math.random() * 3);
+                let col = Math.floor(Math.random() * 3);
+                while (board.currentBoard[row][col] !== null) {
+                     row = Math.floor(Math.random() * 3);
+                     col = Math.floor(Math.random() * 3);
+                }
+                plays.markBoard(row, col);
+                plays = human;
+            }
+        }
 
     }
 
@@ -21,23 +47,21 @@ const game = (function () {
     }
 })()
 
-function player(mark) {
+function player(name,mark) {
     const markBoard = function (row, col) {
         if (board.currentBoard[row][col] === null) {
             board.currentBoard[row][col] = mark;
-            console.log(board.currentBoard);
-        }else {
-            console.log("It's full");
-            console.log(board.currentBoard);
+            alert(board.currentBoard);
         }
     }
 
     return {
+        name,
         mark,
         markBoard,
     }
 }
 
-const player1 = player("X");
-const player2 = player("O");
+const human = player("Human", "X");
+const computer = player("Computer", "O");
 document.querySelector("button").addEventListener("click",game.playGame) 
