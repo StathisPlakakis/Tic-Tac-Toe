6025a9ee1a749b81cdaa961278dfd4e169252061
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-15 16:21:21 +0300

    Make significant changes to the codebase
    1Deleted player factory function 2Made game objject really simple 3Add display object so that the grid of the game is visble

--- assets/script.js
+++ assets/script.js
@@ -67,81 +67,98 @@ const board = (function () {
     }
 })();
 
-function player(name,mark) {
-    const markBoard = function (row, col) {
-        if (board.currentBoard[row][col] === null) {
-            board.currentBoard[row][col] = mark;
-            alert(board.currentBoard);
-        }
-    }
 
-    return {
-        name,
-        mark,
-        markBoard,
+
+const display = (function () {
+    const container = document.createElement("div");
+    container.style.width = "450px";
+    container.style.height = "450px";
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = "1fr 1fr 1fr";
+    container.style.gridAutoRows = "150px";
+    document.body.appendChild(container);
+
+
+    const render = function () {
+        container.innerHTML = "";
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement("div");
+                cell.style.backgroundColor = "lightblue";
+                cell.style.border = "1px solid";
+                cell.style.textAlign = "center";
+                cell.setAttribute("i", i);
+                cell.setAttribute("j", j);
+                cell.addEventListener("click", (e) => {
+                    if (game.isActive) {
+                        const pickRow = e.target.getAttribute("i");
+                        const pickCol = e.target.getAttribute("j");
+                        
+                        if (board.currentBoard[pickRow][pickCol] === null) {
+                            board.currentBoard[pickRow][pickCol] = "X";
+                            render();
+                            game.endGame("X");
+                            if (game.isActive) {
+                                let row = Math.floor(Math.random() * 3);
+                                let col = Math.floor(Math.random() * 3);
+                                while (board.currentBoard[row][col] !== null) {
+                                     row = Math.floor(Math.random() * 3);
+                                     col = Math.floor(Math.random() * 3);
+                                }
+                                board.currentBoard[row][col] = "O";
+                                render();
+                                game.endGame("O");
+                            }else {
+                                console.log(game.winner)
+                            }
+                        }
+                    }else {
+                        console.log(game.winner)
+                    }
+                      
+                    
+                })
+                if (board.currentBoard[i][j] === null) {
+                    cell.textContent = "";
+                }else if (board.currentBoard[i][j] === "X") {
+                    cell.textContent = "X";
+                }else {                    
+                    cell.textContent = "O";                
+                }
+                container.appendChild(cell);
+            } 
+        }   
     }
-}
+    return {
+        render,
+    }
+})();
 
 const game = (function () {
-    const playGame = function () {
-        console.log("Let's Play!!")
-        console.log(board.currentBoard);
+    let isActive = true;
+    let winner;
 
-        const human = player("Human", "X");
-        const computer = player("Computer", "O");
-        let plays = human;
-        let gameActive = true;
-        let winner = null;
-        const action = function (plays, row, col) {
-            plays.markBoard(row, col);            
-            if (board.checkWinner(plays.mark)) {
-                winner = plays;
-                gameActive = false;
-            }else if (board.checkDraw()) {
-                gameActive = false;
-
-            }
-            
-        };
-
-        while (gameActive) {
-            console.log(`${plays.name} plays`);
-            if (plays === human) {
-                let row = prompt("row: ");
-                let col = prompt("col: ");
-                while (board.currentBoard[row][col] !== null) {
-                    alert("This cell is full pick another")
-                    row = prompt("row: "); 
-                    col = prompt("col: ");
-               }
-                action(plays, row,col);
-                plays = computer;
-            }else {
-                let row = Math.floor(Math.random() * 3);
-                let col = Math.floor(Math.random() * 3);
-                while (board.currentBoard[row][col] !== null) {
-                     row = Math.floor(Math.random() * 3);
-                     col = Math.floor(Math.random() * 3);
-                }
-                action(plays, row,col);
-                plays = human;
-
-            }
+    const endGame = function (mark) {
+        if (board.checkWinner(mark)) {
+            game.winner = mark;
+           game.isActive = false;
+        }else if (board.checkDraw()) {
+            game.winner = null;
+            game.isActive = false;
         }
-
-        console.log(winner);
-
     }
 
     return {
-        playGame,
+        isActive,
+        winner,
+        endGame,
     }
 })()
 
 
+display.render()
 
 
-document.querySelector("button").addEventListener("click",
-game.playGame) 
 
 
